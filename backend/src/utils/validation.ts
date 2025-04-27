e5596e4f7ed140d0b25c40f8e0b50778efb7408e
Emmanueloye <esunday963@gmail.com>
2025-04-27 21:12:01 +0100

    validate update

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,53 +1,33 @@
 import * as AppError from '../errors/appError';
 import { NextFunction, Request, Response } from 'express';
 import { Types } from 'mongoose';
-const { param } = require('express-validator');
+const { param, validationResult } = require('express-validator');
 
-// const { body, param, validationResult } = expressValidator;
-
-export const checkForErrors = (validations: any, errType = 400) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    // sequential processing, stops running validations chain if one fails.
-    for (const validation of validations) {
-      const result = await validation.run(req);
-      if (!result.isEmpty()) {
-        const errorMsg = result
-          .array()
-          .map((val: { msg: any }) => val.msg)
-          .join(',');
-        throw new AppError.BadRequest(errorMsg);
+export const checkForErrors = (validatedInputs: any, errType = 400) => {
+  return [
+    validatedInputs,
+    (req: Request, res: Response, next: NextFunction) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        if (errType === 400) {
+          const errMsg = errors
+            .array()
+            .map((val: { msg: string }) => val.msg)
+            .join(',');
+          throw new AppError.BadRequest(errMsg);
+        }
+        if (errType === 401) {
+          const errMsg = errors
+            .array()
+            .map((val: { msg: string }) => val.msg)
+            .join(',');
+          throw new AppError.Unauthenticated(errMsg);
+        }
       }
-    }
-
-    next();
-  };
+      next();
+    },
+  ];
 };
-
-// export const checkForErrors = (validatedInputs: any, errType = 400) => {
-//   return [
-//     validatedInputs,
-//     (req: Request, res: Response, next: NextFunction) => {
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         if (errType === 400) {
-//           const errMsg = errors
-//             .array()
-//             .map((val) => val.msg)
-//             .join(',');
-//           throw new AppError.BadRequest(errMsg);
-//         }
-//         if (errType === 401) {
-//           const errMsg = errors
-//             .array()
-//             .map((val) => val.msg)
-//             .join(',');
-//           throw new AppError.Unauthenticated(errMsg);
-//         }
-//       }
-//       next();
-//     },
-//   ];
-// };
 
 export const validateParams = checkForErrors([
   param('id')
