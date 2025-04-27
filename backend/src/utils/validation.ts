d8a27ce32d155949120263d379523b3ddf46fbde
Emmanueloye <esunday963@gmail.com>
2025-04-27 20:48:12 +0100

    validate update

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,41 +1,54 @@
-import {
-  body,
-  validationResult,
-  param,
-  ContextRunner,
-} from 'express-validator';
 import * as AppError from '../errors/appError';
 import { NextFunction, Request, Response } from 'express';
+import expressValidator = require('express-validator');
 import { Types } from 'mongoose';
 
-export const checkForErrors = (validatedInputs: any, errType = 400) => {
-  return [
-    validatedInputs,
-    (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        if (errType === 400) {
-          const errMsg = errors
-            .array()
-            .map((val) => val.msg)
-            .join(',');
-          throw new AppError.BadRequest(errMsg);
-        }
-        if (errType === 401) {
-          const errMsg = errors
-            .array()
-            .map((val) => val.msg)
-            .join(',');
-          throw new AppError.Unauthenticated(errMsg);
-        }
+export const checkForErrors = (validations: any, errType = 400) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    // sequential processing, stops running validations chain if one fails.
+    for (const validation of validations) {
+      const result = await validation.run(req);
+      if (!result.isEmpty()) {
+        const errorMsg = result
+          .array()
+          .map((val: { msg: any }) => val.msg)
+          .join(',');
+        throw new AppError.BadRequest(errorMsg);
       }
-      next();
-    },
-  ];
+    }
+
+    next();
+  };
 };
 
-export const validateParams = checkForErrors([
-  param('id')
-    .custom((value) => Types.ObjectId.isValid(value))
-    .withMessage('No page found for the requested id'),
-]);
+// export const checkForErrors = (validatedInputs: any, errType = 400) => {
+//   return [
+//     validatedInputs,
+//     (req: Request, res: Response, next: NextFunction) => {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         if (errType === 400) {
+//           const errMsg = errors
+//             .array()
+//             .map((val) => val.msg)
+//             .join(',');
+//           throw new AppError.BadRequest(errMsg);
+//         }
+//         if (errType === 401) {
+//           const errMsg = errors
+//             .array()
+//             .map((val) => val.msg)
+//             .join(',');
+//           throw new AppError.Unauthenticated(errMsg);
+//         }
+//       }
+//       next();
+//     },
+//   ];
+// };
+
+// export const validateParams = checkForErrors([
+//   param('id')
+//     .custom((value) => Types.ObjectId.isValid(value))
+//     .withMessage('No page found for the requested id'),
+// ]);
